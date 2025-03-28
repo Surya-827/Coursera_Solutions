4ff189b70d02812a019d7a172f303c59e4cba631
Surya-0827 <vsuryakiran827@gmail.com>
2025-03-28 19:13:35 +0530

    Solutions for SQL Basic skill certificate course

---
+++ HackerRank-SQL-Basic-Skill-Certificate/merit-rewards.sql
@@ -0,0 +1,15 @@
+SELECT
+    ei.emplyee_ID,
+    ei.name
+FROM
+    employee_information ei
+JOIN
+    last_quarter_bonus b
+ON
+    b.employee_ID = ei.employee_ID
+WHERE
+    ei.division
+LIKE
+    'HR'
+AND
+    b.bonus >= 5000;
